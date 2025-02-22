1f691301eb48514b7dc7226c3ea117b9194b439a
Chirag Atal <chirag.atal@espressif.com>
2025-02-22 14:27:57 +0530

    Database: Setup Prisma schema with Post model and migrations

--- prisma/migrations/20250222075837_init/migration.sql
+++ prisma/migrations/20250222075837_init/migration.sql
@@ -4,6 +4,13 @@ CREATE TABLE "Post" (
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "ingredients" TEXT NOT NULL,
+    "steps" TEXT NOT NULL,
+    "notes" TEXT,
+    "images" TEXT NOT NULL,
+    "tags" TEXT,
+    "category" TEXT,
+    "cookingTime" INTEGER,
+    "difficulty" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "isPublic" BOOLEAN NOT NULL DEFAULT false
